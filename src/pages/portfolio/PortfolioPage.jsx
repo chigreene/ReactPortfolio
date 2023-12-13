7b9ccb42c696023e6fc75f461cb9bb1550017d4b
Christopher Greene <chigreenwork@gmail.com>
2023-12-13 12:16:15 -0500

    fills out profileData.js with data for the portfolio cards. Cards render properly, still need to modify size and position of cards and add pictures for links

--- src/pages/portfolio/PortfolioPage.jsx
+++ src/pages/portfolio/PortfolioPage.jsx
@@ -1,7 +1,47 @@
+import profileData from "../../profileData";
+import seasonalCatalogBackground from "../../assets/seasonalCatalogBackground.jpg";
+
 function PortfolioPage() {
   return (
     <>
       <h2>Portfolio Page</h2>
+      <ul className="list-group">
+        {profileData.map((data) => (
+          <li className="list-group-item" key={data.id}>
+            <div
+              className="card"
+              style={{
+                backgroundImage: `url(${data.backgroundImg})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                color: "white",
+              }}
+            >
+              <div className="card-body">
+                <h5 className="card-title">{data.title}</h5>
+                <a
+                  href={data.gitHubLink}
+                  className="card-link"
+                  target="_blank"
+                  // noopener prevents the new window from having access to the window.opener property and the DOM of the opener window
+                  // noreferrer prevents the new page from being able to access information from the original page
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>
+                <a
+                  href={data.deployedLink}
+                  className="card-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Deployed App
+                </a>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
---
+++ src/profileData.js
@@ -0,0 +1,54 @@
+import seasonalCatalogBackground from "./assets/seasonalCatalogBackground.jpg";
+import techBlogBackground from "./assets/techBlogBackground.png";
+import wordOrJokeBackground from "./assets/wordOrJokeBackground.jpg";
+import weatherAPIBackground from "./assets/weatherAPIBackground.jpg";
+import workDayBackground from "./assets/workDayBackground.jpg";
+import firstPortfolioBackground from "./assets/firstPortfolioBackground.jpg";
+
+const profileData = [
+  {
+    id: 1,
+    title: "Seasonal Sales Catalog",
+    gitHubLink: "https://github.com/chigreene/Seasonal-Sales-Catalog",
+    deployedLink: "https://seasonalcatalog-625ddbfce742.herokuapp.com/login",
+    backgroundImg: seasonalCatalogBackground,
+  },
+  {
+    id: 2,
+    title: "Tech Blog",
+    gitHubLink: "https://github.com/chigreene/MVC-TechBlog",
+    deployedLink: "https://hidden-retreat-30336-abed351f7921.herokuapp.com/",
+    backgroundImg: techBlogBackground,
+  },
+  {
+    id: 3,
+    title: "To Word or Joke",
+    gitHubLink: "https://github.com/chigreene/To-Word-or-To-Joke",
+    deployedLink:
+      "https://chigreene.github.io/To-Word-or-To-Joke/joke/programmingSetUpJokes/index.html",
+    backgroundImg: wordOrJokeBackground,
+  },
+  {
+    id: 4,
+    title: "Weather API WebPage",
+    gitHubLink: "https://github.com/chigreene/Weather-API-WebPage",
+    deployedLink: "https://chigreene.github.io/Weather-API-WebPage/",
+    backgroundImg: weatherAPIBackground,
+  },
+  {
+    id: 5,
+    title: "Workday Scheduler",
+    gitHubLink: "https://github.com/chigreene/Work-Day-Scheduler",
+    deployedLink: "https://chigreene.github.io/Work-Day-Scheduler/",
+    backgroundImg: workDayBackground,
+  },
+  {
+    id: 6,
+    title: "First Portfolio WebPage",
+    gitHubLink: "https://github.com/chigreene/Beach-Themed-Portfolio",
+    deployedLink: "https://chigreene.github.io/Beach-Themed-Portfolio/",
+    backgroundImg: firstPortfolioBackground,
+  },
+];
+
+export default profileData;
