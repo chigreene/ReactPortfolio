bcf29d53b4e7549aada244cfcdca6e881af6fc70
Christopher Greene <chigreenwork@gmail.com>
2023-12-13 17:36:49 -0500

    fixes bottom margin of pages

--- src/pages/portfolio/portfolioPage.jsx
+++ src/pages/portfolio/portfolioPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import profileData from "../../profileData";
+import "./style.css";
 
 function PortfolioPage() {
   useEffect(() => {
@@ -12,46 +13,48 @@ function PortfolioPage() {
 
   return (
     <>
-      <h2>Portfolio Page</h2>
-      <ul className="list-group">
-        {profileData.map((data) => (
-          <li className="list-group-item" key={data.id}>
-            <div
-              className="card"
-              style={{
-                backgroundImage: `url(${data.backgroundImg})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                color: "white",
-              }}
-            >
-              <div className="card-body">
-                <h5 className="card-title">{data.title}</h5>
-                <a
-                  href={data.gitHubLink}
-                  className="card-link"
-                  target="_blank"
-                  // noopener prevents the new window from having access to the window.opener property and the DOM of the opener window
-                  // noreferrer prevents the new page from being able to access information from the original page
-                  rel="noopener noreferrer"
-                  style={{ color: "#f8f8f8" }}
-                >
-                  GitHub
-                </a>
-                <a
-                  href={data.deployedLink}
-                  className="card-link"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  style={{ color: "#f8f8f8" }}
-                >
-                  Deployed App
-                </a>
+      <div className="container">
+        <h2>Portfolio Page</h2>
+        <ul className="list-group">
+          {profileData.map((data) => (
+            <li className="list-group-item" key={data.id}>
+              <div
+                className="card"
+                style={{
+                  backgroundImage: `url(${data.backgroundImg})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  color: "white",
+                }}
+              >
+                <div className="card-body">
+                  <h5 className="card-title">{data.title}</h5>
+                  <a
+                    href={data.gitHubLink}
+                    className="card-link"
+                    target="_blank"
+                    // noopener prevents the new window from having access to the window.opener property and the DOM of the opener window
+                    // noreferrer prevents the new page from being able to access information from the original page
+                    rel="noopener noreferrer"
+                    style={{ color: "#f8f8f8" }}
+                  >
+                    GitHub
+                  </a>
+                  <a
+                    href={data.deployedLink}
+                    className="card-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ color: "#f8f8f8" }}
+                  >
+                    Deployed App
+                  </a>
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
